1c12ec74db92980c01ea580c24b0fa2454eec6bc
aribkx <66409119+aribkx@users.noreply.github.com>
2020-07-06 15:26:02 -0700

    Add files via upload

---
+++ storage.js
@@ -0,0 +1,202 @@
+
+
+
+
+var myRoster = [
+
+{
+        "name" : "Bernad Leno",
+        "number" : "1"
+}
+,
+{
+    "name" : "Emiliano Martinez",
+    "number" : "26"
+}
+,
+{
+"name" : "Matt Macey",
+"number" : "33"
+}
+,
+{
+"name" : "Hector Bellerin",
+"number" : "2",
+}
+,
+{
+"name" : "Kieran Tierney",
+"number" : "3",
+}
+,
+{
+"name" : "Sokratis Papastathopoulos",
+"number" : "5",
+}
+,
+{
+"name" : "Rob Holding",
+"number" : "16",
+}
+,
+{
+"name" : "Shkodran Mustafi",
+"number" : "20",
+}
+,
+{
+"name" : "Pablo Mari",
+"number" : "22",
+}
+,
+{
+"name" : "David Luiz",
+"number" : "23",
+}
+,
+{
+"name" : "Sead Kolasinac",
+"number" : "31",
+}
+,
+{
+"name" : "Tolaji Bola",
+"number" : "40",
+}
+,
+{
+"name" : "Zech Medley",
+"number" : "49",
+}
+,
+{
+"name" : "Bukayo Saka",
+"number" : "77",
+}
+,
+{
+"name" : "Dani Ceballos",
+"number" : "8",
+}
+,
+{
+"name" : "Mesut Ozil",
+"number" : "10",
+}
+,
+{
+"name" : "Lucas Torreria",
+"number" : "11",
+}
+,
+{
+"name" : "Ainsley Maitland-Niles",
+"number" : "15",
+}
+,
+{
+"name" : "Cedric Soares",
+"number" : "17",
+}
+,
+{
+"name" : "Calum Chambers",
+"number" : "21",
+}
+,
+{
+"name" : "Joseph Willock",
+"number" : "28",
+}
+,
+{
+"name" : "Matteo Guendouzi",
+"number" : "29",
+}
+,
+{
+"name" : "Granit Xhaka",
+"number" : "34",
+}
+,
+{
+"name" : "Gabriel Martinelli",
+"number" : "35",
+}
+,
+{
+"name" : "Robbie Burton",
+"number" : "41",
+}
+,
+{
+"name" : "Alexandre Lazazette",
+"number" : "9",
+}
+,
+{
+"name" : "Pierre-Emerick Aubameyang",
+"number" : "14",
+}
+,
+{
+"name" : "Nicolas Pepe",
+"number" : "19",
+}
+,
+{
+"name" : "Reiss Nelson",
+"number" : "24",
+}
+,
+{
+"name" : "Edward Nketiah",
+"number" : "30",
+}
+,
+{
+"name" : "Folarian Balogun",
+"number" : "38",
+}
+
+];
+var gfd = Math.floor(Math.random() * myRoster.length);
+var fdd = Math.floor(Math.random() * myRoster.length);
+document.getElementById("firstposition").innerHTML="<h1>"+myRoster[gfd].number+" "+myRoster[gfd].name+"</h1>";
+functon
+var fsa = fsa+1
+
+
+var myApperal = [
+
+{
+        "name" : "Bernad Leno",
+        "number" : "1"
+}
+,
+{
+    "name" : "Emiliano Martinez",
+    "number" : "26"
+}
+,
+{
+"name" : "Matt Macey",
+"number" : "33"
+}
+,
+{
+"name" : "Hector Bellerin",
+"number" : "2",
+}
+,
+{
+"name" : "Kieran Tierney",
+"number" : "3",
+}
+,
+{
+"name" : "Sokratis Papastathopoulos",
+"number" : "5",
+
+}
+];
